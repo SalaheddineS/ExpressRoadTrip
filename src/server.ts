b2559ce5d211616aa39ccb41e5aa65e52b023238
Salaheddine <Salaheddinemorchid1@gmail.com>
2023-07-11 10:50:49 +0100

    added Json parser Middleware

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,14 @@
 import express, { Express,Request, Response } from 'express';
+import { Interface } from 'readline';
 
 const app:Express = express();
 const port=3000;
+app.use(express.json());
+
+interface User {
+    name:string,
+    age:Number
+}
 
 app.get('/',(req:Request,res:Response)=>
 {
@@ -9,5 +16,12 @@ res.status(201).json({name:"Road",lastName:"Trip"});
 }
 )
 
+app.post('/post',(req:Request,res:Response)=>
+{
+    const body:User = req.body
+    const userInfo:User={name:body.name,age:body.age};
+    res.status(201).json(userInfo);
+}
+)
 
 app.listen(port,()=>{console.log("the Road Trip server is running")});
